607176e956b78440e307efddf5afbc109c7e554e
Kale-Francis <kalefrancis989@gmail.com>
2025-06-20 10:16:59 +0000

    Task 9: Fix auth.py with _generate_uuid, Python 3.7 annotations, and pycodestyle

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,10 +3,12 @@
 Authentication module
 """
 import bcrypt
-from db import DB
-from user import User
-from sqlalchemy.orm.exc import NoResultFound
 import uuid
+from typing import Union
+
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -25,7 +27,7 @@ def _generate_uuid() -> str:
     """Generate a new UUID
 
     Returns:
-        str: String representation of a new UUID
+        str: String representation of a UUID
     """
     return str(uuid.uuid4())
 
@@ -58,7 +60,7 @@ class Auth:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
 
-    def valid_login(self, email, password: str) -> bool:
+    def valid_login(self, email: str, password: str) -> bool:
         """Validate user credentials
 
         Args:
@@ -69,41 +71,8 @@ class Auth:
             bool: True if credentials are valid, False otherwise
         """
         try:
-            user = self._db(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
         except NoResultFound:
             return False
-
-    def create_session(self, email: str) -> str:
-        """Create a session for a user
-
-        Args:
-            email (str): User's email
-
-        Returns:
-            str: Session ID as a string, or None if user not found
-        """
-        try:
-            user = self._db(email=email)
-            session_id = _generate_uuid()
-            self._db.update_user(user.id, session_id=session_id)
-            return session_id
-        except NoResultFound:
-            return None
-
-
-    def get_user_from_session_id(self, session_id: str) -> User | None:
-        """Find user by session ID
-
-        Args:
-            session_id (str): Session ID
-
-        Returns:
-            User | None: Corresponding User object or None if not found
-        """
-        if session_id is None:
-            return None
-        try:
-            return self._db.find_user_by(session_id=session_id)
-        except NoResultFound:
-            return None
