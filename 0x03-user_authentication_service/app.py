ae10b503630c62523bad7d0402d3e136a22e9440
Kale-Francis <kalefrancis989@gmail.com>
2025-06-20 09:32:15 +0000

    Task 8: Update auth.py with valid_login method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,39 +1,64 @@
 #!/usr/bin/env python3
 """
-Basic Flask app
+Authentication module
 """
-from flask import Flask, jsonify, request
-from auth import Auth
-
-app = Flask(__name__)
-AUTH = Auth()
-
-
-@app.route('/', methods=['GET'])
-def welcome() -> dict:
-    """GET route that returns a welcome message
-
-    Returns:
-        dict: JSON payload with welcome message
-    """
-    return jsonify({"message": "Bienvenue"})
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
-@app.route('/users', methods=['POST'])
-def users() -> dict:
-    """POST /users route to register a new user
+def _hash_password(password: str) -> bytes:
+    """Hash a password using bcrypt
+
+    Args:
+        password (str): Password to hash password
 
     Returns:
-        dict: JSON payload with email and message
+        bytes: Salted and hashed password
     """
-    email = request.form.get('email')
-    password = request.form.get('password')
-    try:
-        AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"})
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user
+
+        Args:
+            email (str): User's email
+            password (str): User's password
+
+        Returns:
+            User: The created User object
+
+        Raises:
+            ValueError: If a user with the email already exists
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate user credentials
+
+        Args:
+            email (str): User's email
+            password (str): User's password
+
+        Returns:
+            bool: True if credentials are valid, False otherwise
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
