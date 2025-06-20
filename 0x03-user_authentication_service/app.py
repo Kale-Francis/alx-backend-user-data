0e835a134acfb1a2cb25452c3122bc84e48c4cfa
Kale-Francis <kalefrancis989@gmail.com>
2025-06-20 16:36:48 +0000

    Task 19: Include auth.py for testing

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -99,5 +99,22 @@ def get_reset_password_token() -> dict:
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> dict:
+    """PUT /reset_password route to update user password
+
+    Returns:
+        dict: JSON payload with email and message
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
