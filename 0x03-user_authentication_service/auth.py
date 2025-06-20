9c7a110d963edadb6e331170f7962faf86a232ba
Kale-Francis <kalefrancis989@gmail.com>
2025-06-20 10:21:21 +0000

    Task 10: Update auth.py with create_session method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,3 +76,20 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> Union[str, None]:
+        """Create a session for a user
+
+        Args:
+            email (str): User's email
+
+        Returns:
+            Union[str, None]: Session ID as a string, or None if user not found
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
