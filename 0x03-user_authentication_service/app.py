502a8a3e59f81c949b7211cf7131e16e7b032155
Kale-Francis <kalefrancis989@gmail.com>
2025-06-20 16:04:10 +0000

    Task 15: Update app.py with GET /profile route for user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -69,5 +69,20 @@ def logout() -> None:
         abort(403)
 
 
+@app.route('/profile', methods=['GET'])
+def profile() -> dict:
+    """GET /profile route to get user profile
+
+    Returns:
+        dict: JSON payload with user email
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email})
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
