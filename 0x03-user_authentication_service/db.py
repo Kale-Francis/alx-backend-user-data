9bdc4621b8e25f16e1663b8362a81ab88546ce0c
Kale-Francis <kalefrancis989@gmail.com>
2025-06-20 08:53:42 +0000

    updated db.py file with the find_user_by method added to the DB class, along with the existing add_user method.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -44,3 +46,24 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword arguments
+
+        Args:
+            **kwargs: Arbitrary keyword arguments to filter the query
+
+        Returns:
+            User: The first user matching the criteria
+
+        Raises:
+            NoResultFound: If no user is found
+            InvalidRequestError: If invalid query arguments are provided
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except TypeError:
+            raise InvalidRequestError
