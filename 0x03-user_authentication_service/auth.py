0a9376a4f16322eb7e474905728052537242a11d
Kale-Francis <kalefrancis989@gmail.com>
2025-06-20 09:09:42 +0000

    Task 5: Update auth.py with register_user method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 Authentication module
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -15,3 +18,31 @@ def _hash_password(password: str) -> bytes:
         bytes: Salted and hashed password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user
+
+        Args:
+            email (str): User's email
+            password (str): User's password
+
+        Returns:
+            User: The created User object
+
+        Raises:
+            ValueError: If a user with the email already exists
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email, hashed_password)
