c6531d544faa41149a002fd718184ffc21cf7709
Kale-Francis <kalefrancis989@gmail.com>
2025-06-20 16:11:07 +0000

    Task 17: Include auth.py for testing

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -84,5 +84,20 @@ def profile() -> dict:
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token() -> dict:
+    """POST /reset_password route to get reset password token
+
+    Returns:
+        dict: JSON payload with email and reset token
+    """
+    email = request.form.get('email')
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
