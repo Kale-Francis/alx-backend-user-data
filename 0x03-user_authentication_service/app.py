0de0da603d44468ef4c6390a0ae50a9e7583bd54
Kale-Francis <kalefrancis989@gmail.com>
2025-06-20 16:01:10 +0000

    Task 14: Update app.py with DELETE /sessions route for logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Basic Flask app
 """
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -51,6 +51,22 @@ def login() -> dict:
         return response
     else:
         abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> None:
+    """DELETE /sessions route to log out a user
+
+    Returns:
+        None: Redirects to GET /
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
